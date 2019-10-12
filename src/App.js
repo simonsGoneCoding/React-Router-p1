e69ed4fc80ef070ee55cab8d03b342c31c4c975c
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-12 09:54:31 +0100

    added working section with Route paths to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
+const Home = () => <h2>Home Page</h2>;
+const News = () => <h2>Latest News</h2>;
+const Contact = () => <h2>Leave me alone</h2>;
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div>
           <header>
             <nav>
@@ -14,20 +18,24 @@ class App extends Component {
                 <li><a href="/news">news</a></li>
                 <li><a href="/contact">contact</a></li> */}
                 <li>
-                  <Link to="/">start</Link>
+                  <Link to="/">Start</Link>
                 </li>
                 <li>
-                  <Link to="/news">news</Link>
+                  <Link to="/news">News</Link>
                 </li>
                 <li>
-                  <Link to="/contact">contact</Link>
+                  <Link to="/contact">Contact</Link>
                 </li>
               </ul>
             </nav>
           </header>
-          <section>Welcome</section>
+          <section>
+            <Route path="/" exact component={Home} />
+            <Route path="/news" component={News} />
+            <Route path="/contact" component={Contact} />
+          </section>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
