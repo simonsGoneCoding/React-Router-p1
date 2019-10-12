ef8c2a9d769e11024de907c85d6f9e3cb1b75c3d
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-12 09:34:27 +0100

    installed react-router-dom and added links to different URLs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <p>ulala</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <ul>
+                {/* <li><a href="/">start</a></li>
+                <li><a href="/news">news</a></li>
+                <li><a href="/contact">contact</a></li> */}
+                <li>
+                  <Link to="/">start</Link>
+                </li>
+                <li>
+                  <Link to="/news">news</Link>
+                </li>
+                <li>
+                  <Link to="/contact">contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <section>Welcome</section>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
