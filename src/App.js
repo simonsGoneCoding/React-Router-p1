76b959decbc325fb602b9b9e27528393fc7c61f3
Szymon Warszawa <szymonwarszawa@gmail.com>
2019-10-13 10:08:57 +0100

    imported Switch component from react-router-dom and added manual error message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-import { BrowserRouter as Router, NavLink, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch
+} from "react-router-dom";
 
 const Home = () => <h2>Home Page</h2>;
 const News = () => <h2>Latest News</h2>;
 const Contact = () => <h2>Leave me alone</h2>;
+
+const ErrorPage = () => <p>This is the end of your journey my friend</p>;
 
 class App extends Component {
   render() {
@@ -33,9 +40,12 @@ class App extends Component {
             </nav>
           </header>
           <section>
-            <Route path="/" exact component={Home} />
-            <Route path="/news" component={News} />
-            <Route path="/contact" component={Contact} />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/news" component={News} />
+              <Route path="/contact" component={Contact} />
+              <Route component={ErrorPage} />
+            </Switch>
           </section>
         </div>
       </Router>
